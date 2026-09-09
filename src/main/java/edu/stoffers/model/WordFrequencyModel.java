8139733a910a85b4b0a6bec1bb9c63429b7fb924
livlivable747 <github.com.livable747@passmail.com>
2026-09-09 15:09:02 +0200

    Update WordFrequencyModel naar record

--- src/main/java/edu/stoffers/model/WordFrequencyModel.java
+++ src/main/java/edu/stoffers/model/WordFrequencyModel.java
@@ -1,14 +1,6 @@
 package edu.stoffers.model;
 
-public class WordFrequencyModel implements WordFrequency {
-
-    private final String word;
-    private final int frequency;
-
-    public WordFrequencyModel(String word, int frequency) {
-        this.word = word;
-        this.frequency = frequency;
-    }
+public record WordFrequencyModel(String word, int frequency) implements WordFrequency {
 
     @Override
     public String getWord() {
